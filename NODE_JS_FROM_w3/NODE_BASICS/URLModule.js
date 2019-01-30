5b6138ec379c4d4b737c338b201f00b78c4a391f
Arturo <grabowski.a93@gmail.com>
2019-01-31 00:43:52 +0100

    URL module from w3Schools

---
+++ NODE_JS_FROM_w3/NODE_BASICS/URLModule.js
@@ -0,0 +1,35 @@
+const url = require('url');
+const http = require('http');
+const fs = require('fs');
+
+http.createServer(function (req, res) {
+    const q = url.parse(req.url,true);
+    const fileName = "." + q.pathname;
+
+    fs.readFile(fileName,function (err, data) {
+        if (err){
+            res.writeHead(404,{'Content-Type': 'text/html'});
+            return res.end("404 Not found");
+        }
+        res.writeHead(200,{'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    })
+}).listen(8080);
+
+
+
+
+
+//first part of tutorial.Parse an address with the url.parse() method, and it will return a URL object with each part of the address as properties:
+/*
+const q = url.parse(adr,true);
+const adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+
+console.log("q.host      : " + q.host); //returns 'localhost:8080'
+console.log("q.pathname  : " + q.pathname); //returns '/default.htm'
+console.log("q.search    : " + q.search); //returns '?year=2017&month=february'
+
+const qdata =  q.query;//returns an object: { year: 2017, month: 'february' }
+console.log("qdata.month : " + qdata.month); //returns 'february'
+*/
