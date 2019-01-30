a17ec8bd64f7811865d8113b7f1f7796a4096638
Arturo <grabowski.a93@gmail.com>
2019-01-30 16:48:01 +0100

    File system module : Read,create,upgrate,delete,rename files

---
+++ NODE_JS_FROM_w3/NODE_BASICS/demo_FileSystemModule.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function (req, res) {
+    //Create a Node.js file that reads the HTML file, and return the content:
+    fs.readFile('demofile1.html', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+    });
+}).listen(8080);
+
+
+// you could find more information on :https://www.w3schools.com/nodejs/nodejs_filesystem.asp
+fs.appendFile('newFile.txt','Ala ma kota2',function (err) {
+    if (err)
+    {
+        throw err;
+    }
+    console.log("saved");
+});
+
+fs.unlink('newFile.txt', function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("File deleted!");
+})
+
+fs.rename('newFile2.txt', 'myFile1.txt',function (err) {
+    if (err){
+        throw err;
+    }
+    console.log('File renamed!');
+
+})
+
+/*// The fs.appendFile() method appends specified content to a file. If the file does not exist, the file will be created:
+fsopen('newFile2.txt','w',function (err, file) {
+    if (err) {
+        throw err;
+    }
+    console.log('newFile2.txt saved!');
+})*/
+
